bdd703a4a998fac9b2b04553be7f90014786452e
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-04-10 17:16:50 +0200

    database with keys as key and not string (only the ones we use)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,41 @@
 import './App.css'
 import ListOfItems from './components/ListOfItems';
 import PopUp from './components/PopUp';
+import { countries } from './databases/countries';
+import { weathers } from './databases/weather';
+
+export interface Country {
+  name: {
+    common: string,
+    official: string,
+  },
+  currencies: object,
+  capital: string[],
+  region: string,
+  subregion: string,
+  languages: object,
+  latlng: [number, number],
+  flag: string,
+  maps: {
+    googleMaps: string,
+    openStreetMaps: string
+  },
+  population: number,
+  gini: number,
+  flags: {
+    png: string,
+    svg: string,
+    alt: string
+  },
+  coatOfArms: {
+    png: string,
+    svg: string
+  },
+  capitalInfo: {
+    latlng: [number, number]
+  },
+}
+
 
 function App() {
 
