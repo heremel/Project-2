fcfc131b8257de3a79619dfc60da3be4c02d1f27
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-04-14 18:13:22 +0200

    one more country

--- src/databases/countries.ts
+++ src/databases/countries.ts
@@ -3202,7 +3202,7 @@ export const countries: Countries = [
         capitalInfo: {
             latlng: [8.48, -13.23]
         }
-    }/*,
+    },
     {
         name: {
             common: "Burkina Faso",
@@ -3211,136 +3211,20 @@ export const countries: Countries = [
         },
 
         currencies: "West African CFA franc",
-        "idd": {
-            "root": "+2",
-            "suffixes": [
-                "26"
-            ]
-        },
+
         capital: [
             "Ouagadougou"
         ],
-        "altSpellings": [
-            "BF"
-        ],
+
         region: "Africa",
         subregion: "Western Africa",
         languages: [
             "French"
         ],
-        "translations": {
-            "ara": {
-                official: "بوركينا فاسو",
-                common: "بوركينا فاسو"
-            },
-            "bre": {
-                official: "Burkina Faso",
-                common: "Burkina Faso"
-            },
-            "ces": {
-                official: "Burkina Faso",
-                common: "Burkina Faso"
-            },
-            "cym": {
-                official: "Bwrcina Ffaso",
-                common: "Bwrcina Ffaso"
-            },
-            "deu": {
-                official: "Burkina Faso",
-                common: "Burkina Faso"
-            },
-            "est": {
-                official: "Burkina Faso",
-                common: "Burkina Faso"
-            },
-            "fin": {
-                official: "Burkina Faso",
-                common: "Burkina Faso"
-            },
-            fra: {
-                official: "République du Burkina",
-                common: "Burkina Faso"
-            },
-            "hrv": {
-                official: "Burkina Faso",
-                common: "Burkina Faso"
-            },
-            "hun": {
-                official: "Burkina Faso",
-                common: "Burkina"
-            },
-            "ita": {
-                official: "Burkina Faso",
-                common: "Burkina Faso"
-            },
-            "jpn": {
-                official: "ブルキナファソ",
-                common: "ブルキナファソ"
-            },
-            "kor": {
-                official: "부르키나파소",
-                common: "부르키나파소"
-            },
-            "nld": {
-                official: "Burkina Faso",
-                common: "Burkina Faso"
-            },
-            "per": {
-                official: "بورکینافاسو",
-                common: "بورکینافاسو"
-            },
-            "pol": {
-                official: "Burkina Faso",
-                common: "Burkina Faso"
-            },
-            "por": {
-                official: "Burkina Faso",
-                common: "Burkina Faso"
-            },
-            "rus": {
-                official: "Буркина -Фасо",
-                common: "Буркина-Фасо"
-            },
-            "slk": {
-                official: "Burkina Faso",
-                common: "Burkina Faso"
-            },
-            "spa": {
-                official: "Burkina Faso",
-                common: "Burkina Faso"
-            },
-            "srp": {
-                official: "Буркина Фасо",
-                common: "Буркина Фасо"
-            },
-            "swe": {
-                official: "Burkina Faso",
-                common: "Burkina Faso"
-            },
-            "tur": {
-                official: "Burkina Faso",
-                common: "Burkina Faso"
-            },
-            "urd": {
-                official: "برکینا فاسو",
-                common: "برکینا فاسو"
-            },
-            "zho": {
-                official: "布基纳法索",
-                common: "布基纳法索"
-            }
-        },
+
         latlng: [13, -2],
         landlocked: true,
-        "borders": [
-            "BEN",
-            "CIV",
-            "GHA",
-            "MLI",
-            "NER",
-            "TGO"
-        ],
-        "area": 272967,
+
         demonyms: {
             eng: {
                 fem: "Burkinabe",
@@ -3358,33 +3242,21 @@ export const countries: Countries = [
         },
         population: 20903278,
         gini: 35.3,
-        , "fifa": "BFA",
-    "car": {
-        "signs": [
-            "BF"
-        ],
-        "side": "right"
-    },
-    "timezones": [
-        "UTC"
-    ],
-    "continents": [
-        "Africa"
-    ],
-    flags: {
-        png: "https://flagcdn.com/w320/bf.png",
-        svg: "https://flagcdn.com/bf.svg",
-        alt: "The flag of Burkina Faso features two equal horizontal bands of red and green, with a yellow five-pointed star in the center."
-    },
-    coatOfArms: {
-        png: "https://mainfacts.com/media/images/coats_of_arms/bf.png",
-        svg: "https://mainfacts.com/media/images/coats_of_arms/bf.svg"
-    },
 
-    capitalInfo: {
-        latlng: [12.37, -1.52]
-    }
-},
+        flags: {
+            png: "https://flagcdn.com/w320/bf.png",
+            svg: "https://flagcdn.com/bf.svg",
+            alt: "The flag of Burkina Faso features two equal horizontal bands of red and green, with a yellow five-pointed star in the center."
+        },
+        coatOfArms: {
+            png: "https://mainfacts.com/media/images/coats_of_arms/bf.png",
+            svg: "https://mainfacts.com/media/images/coats_of_arms/bf.svg"
+        },
+
+        capitalInfo: {
+            latlng: [12.37, -1.52]
+        }
+    }/*,
 {
     name: {
         common: "Eritrea",
