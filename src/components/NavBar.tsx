a782257608adb779dfd499b58c62c96d187c3637
Ernest-D <daphnee.ernest@yahoo.com>
2025-04-29 15:26:14 +0200

    CSS_NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router";
+import style from "../assets/styles/NavBar.module.css";
+
 
 function NavBar() {
 	return (
 		<>
-			<nav>
-				<Link to="/">About</Link>
-				<Link to="/search">Research</Link>
+			<nav className={style.navbar}>
+				<a href="/"><img src="../assets/pictogram/picto_home.svg" alt="home" /></a>
+				<a href="/search"><img src="../assets/pictogram/picto_loupe.svg" alt="search" /></a>
 			</nav>
 		</>
 	);
