23047752bd3fd0977138a2c20fc9ecda3d0dba2a
Ernest-D <daphnee.ernest@yahoo.com>
2025-05-05 17:43:02 +0200

    active navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,20 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router-dom";
 import style from "../assets/styles/NavBar.module.css";
 
+const navIcon = [
+	{to: "/", src: "/src/assets/pictogram/picto_home.svg", alt: "home"},
+	{to: "/search", src: "/src/assets/pictogram/picto_loup.svg", alt: "search"},
+	{to: "/favorite", src: "/src/assets/pictogram/picto_heart.svg", alt: "favorite"}
+]
 
 function NavBar() {
 	return (
 		<>
 			<nav className={style.navbar}>
-			<Link to="/"><img src="/src/assets/pictogram/picto_home.svg" alt="home" /></Link> 
-			<Link to="/search"><img src="/src/assets/pictogram/picto_loup.svg" alt="search" /></Link>
-			<Link to="/favorite"><img src="/src/assets/pictogram/picto_heart.svg" alt="my list" /></Link>
+  			{navIcon.map((icon) => (
+    			<NavLink  key={icon.to} to={icon.to}  className={({ isActive }) => isActive ? `${style.navIcon} ${style.active}` : style.navIcon}>
+      				<img src={icon.src} alt={icon.alt} />
+    			</NavLink>))}
 			</nav>
 		</>
 	);
