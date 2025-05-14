d4a4abbf6c06728ce8000899f1d6f627ba9788e7
heremel <melissaferreira2510@gmail.com>
2025-05-14 17:19:08 +0200

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// 👇 Ajoute ceci si ton dépôt s'appelle "mon-projet"
 export default defineConfig({
-  plugins: [react()],
-})
+	base: "/Project-2/",
+	plugins: [react()],
+});
